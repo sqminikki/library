d1d9da1ff1fe33546fa1091219acef51c23abb8e
sqminikki <148020252+sqminikki@users.noreply.github.com>
2024-03-29 16:26:50 -0500

    Add files via upload

---
+++ cross-compiling-partone/build/CMakeFiles/hello.dir/hello.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/hello.dir/hello.c.o: /app/hello.c \
+ /duo-sdk/rootfs/usr/include/stdc-predef.h \
+ /duo-sdk/rootfs/usr/include/stdio.h \
+ /duo-sdk/rootfs/usr/include/features.h \
+ /duo-sdk/rootfs/usr/include/bits/alltypes.h
