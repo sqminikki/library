187d927c3a5c8f28c155a0821e8b022fbdf267b0
sqmi_ <ssammie47@gmail.com>
2024-04-16 16:15:13 -0500

    added OpenSSL

---
+++ Embedded-Systems-Design/mqtt/cJSON-1.7.17/build/CMakeFiles/cjson-static.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjson-static.dir/cJSON.c.o"
+  "CMakeFiles/cjson-static.dir/cJSON.c.o.d"
+  "libcjson.a"
+  "libcjson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cjson-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Embedded-Systems-Design/mqtt/cJSON-1.7.17/build/CMakeFiles/cjson-static.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cjson-static.
+# This may be replaced when dependencies are built.
